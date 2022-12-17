e33abd2789c744b5fc409c2a1d52b48a91465378
Miftahul Fajri Sidqi <miftahulfajrisidqi529@gmail.com>
2022-12-17 23:13:21 +0700

    upload apps photos

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://photo-gallery-apps.web.app/" // TODO: replace this
+const studentName = "Miftahul Fajri S" // TODO: replace this
+const studentId = "FE3251802" // TODO: replace this
+
+export { url, studentName, studentId }
